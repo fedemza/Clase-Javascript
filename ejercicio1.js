a80e88e4b3d9b4166e6fca5ad7794cca21f7ef51
Federico Voloschin <fedevoloschin@gmail.com>
2021-08-19 11:48:32 -0300

    ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,41 @@
+// let clase='numen'
+// console.log(clase)
+
+// let ejemplo='prueba'
+// console.log(ejemplo)
+// let verdad= true ;
+// let numero= 8
+
+// let arreglo=['texto1', 8888, true]
+// console.log(numero,ejemplo,verdad,arreglo)
+
+
+// function operacion(n1,n2){
+//     console.log(n1+n2)
+// }
+// operacion(3,4)
+
+// operacion(numero,2)
+
+// let nombre= 'Fede'
+// let apellido= 'Voloschin';
+
+// function nombrecompl(var1,var2){
+//     console.log(var1+' '+var2)
+
+// }
+// nombrecompl(nombre, 'apellido')
+let animal= 'Jirafa'
+
+
+
+
+let numero1= animal.length
+console.log(numero1)
+
+let resultado
+
+resultado= Math.round((Math.pow(numero1,3))-10.7)
+
+console.log(resultado)
+
